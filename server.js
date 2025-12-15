64c93f24743ee066aefe61dac2c34b03f6dbf561
incmak <ahmedmuieen@gmail.com>
2025-12-15 12:02:27 +0530

    feat: connect to mongodb and envs

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config.env' });
 import app from './app.js';
 
-const PORT = 3000;
+console.log(process.env.NODE_ENV);
+
+mongoose
+	.connect(process.env.DB_URL)
+	.then(() => {
+		console.log('Database connected ✅');
+	})
+	.catch((err) => {
+		console.log('Database connection failed 🔥', err);
+	});
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
